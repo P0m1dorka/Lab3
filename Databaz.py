62dae909b064002cec51b651f7f6a96b829acf82
P0m1dorka <loginovandrej31@gmail.com>
2024-03-13 14:32:15 +0300

    add databaze

--- Databaz.py
+++ Databaz.py
@@ -1,47 +1,66 @@
+class Databaze():
+    user = ["root", "user_dva", "nothing"]
+    password = ["root777", "users", "ayanami"]
+    type = ["admin", "VIP", "classic"]
+    try_user: str
+    try_pass: str
+
+    def __init__(self, _pass: str, _user: str):
+        self.try_pass=_pass
+        self.try_user=_user
+
+
 class Exeption():
-    def throw_exeption(self, text:str) -> str:
+    def throw_exeption(self, text: str) -> str:
         raise NotImplementedError()
+
+
 class NoExeption(Exeption):
-    def throw_exeption(self, text:str) -> str:
+    def throw_exeption(self, text: str) -> str:
         return text
+
 
 class ExeptionChain(Exeption):
     next_exeption: Exeption()
 
     def __init__(self):
-        self.next_exeption=NoExeption()
+        self.next_exeption = NoExeption()
 
-    def set_next(self, next_exe:Exeption):
-        self.next_exeption=next_exe
+    def set_next(self, next_exe: Exeption):
+        self.next_exeption = next_exe
 
-    def throw_exeption(self, text:str) -> str:
+    def throw_exeption(self, text: str) -> str:
         t = self.do_exeption_conc(text)
         return self.next_exeption.throw_exeption(t)
 
-    def do_exeption_conc(self, text:str) -> str:
+    def do_exeption_conc(self, text: str) -> str:
         raise NotImplementedError()
 
-class AvExeption(ExeptionChain):
-    def do_exeption_conc(self, text:str) -> str:
-        print("av")
+
+class AvtorizationExeption(ExeptionChain):
+    def do_exeption_conc(self, text: str) -> str:
+
         return "avexeprtion"
 
-class AuExeption(ExeptionChain):
-    def do_exeption_conc(self, text:str) -> str:
+
+class AuthenticateExeption(ExeptionChain):
+    def do_exeption_conc(self, text: str) -> str:
         print("ai")
         return "auexeption"
 
-class InExeption(ExeptionChain):
 
-    def do_exeption_conc(self, text:str) -> str:
+class InputExeption(ExeptionChain):
+
+    def do_exeption_conc(self, text: str) -> str:
         print("int")
         return "inexeption"
 
-av = AvExeption()
-au = AuExeption()
-ie = InExeption()
+#
+#av = AvtorizationExeption()
+##au = AuthenticateExeption()
+#ie = InputExeption()
 
-av.set_next(au)
-au.set_next(ie)
+#av.set_next(au)
+#au.set_next(ie)
 
-print(av.throw_exeption('test'))
+#print(av.throw_exeption('test'))
--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import random
 import psycopg2
-
+from Databaz import Databaze
 user=input("Type name to connect")
-bd_name=input("Type bd name")
 user_password=input("Type bd password")
+bd_connect = Databaze(user_password,user)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 #conn = psycopg2.connect(database=bd_name, user=user, password=user_password, host="127.0.0.1", port="5432")
